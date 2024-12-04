e88a57c871a2d3ed282da9a216b0519687911f34
heehun <slrkqk2@naver.com>
2024-12-04 13:30:46 +0900

    feat:excludePathPatterns에 swagger api url 추가

--- joongo/src/main/java/com/dbProject/joongo/global/config/WebConfig.java
+++ joongo/src/main/java/com/dbProject/joongo/global/config/WebConfig.java
@@ -17,6 +17,6 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(loginCheckInterceptor)
                 .addPathPatterns("/**")  // 적용할 URL 패턴
                 .order(1)
-                .excludePathPatterns("/api/auth/**"); // 제외할 URL 패턴
+                .excludePathPatterns("/api/auth/**", "/swagger-ui/**", "/v3/**"); // 제외할 URL 패턴
     }
 }
