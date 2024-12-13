00ae2c2486b9466c31a20079651619fc2a407678
heehun <slrkqk2@naver.com>
2024-12-13 15:34:54 +0900

    feat: s3
    file null 로 넘어옴

--- joongo/src/main/java/com/dbProject/joongo/controller/ProductController.java
+++ joongo/src/main/java/com/dbProject/joongo/controller/ProductController.java
@@ -6,15 +6,19 @@ import com.dbProject.joongo.dto.product.ProductResponse;
 import com.dbProject.joongo.service.ProductService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,8 +27,9 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @PostMapping("/")
-    public ResponseEntity<String> addProduct(@RequestBody ProductRequest.ProductInfo request) {
+    @PostMapping(value = "/", consumes = "multipart/form-data")
+    public ResponseEntity<String> addProduct(
+            @ModelAttribute ProductRequest.uploadInfo request)     {
         productService.create(request);
 
         return ResponseEntity.ok("Product created");
--- joongo/src/main/java/com/dbProject/joongo/dto/product/ProductRequest.java
+++ joongo/src/main/java/com/dbProject/joongo/dto/product/ProductRequest.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 public class ProductRequest {
 
@@ -22,6 +23,52 @@ public class ProductRequest {
         private Integer price;
         private Integer categoryId;
         private Integer userId;
+
+        public Product toEntity() {
+            return Product.builder()
+                    .title(this.title)
+                    .content(this.content)
+                    .image(this.image)
+                    .productStatus(this.productStatus)
+                    .location(this.location)
+                    .count(this.count)
+                    .price(this.price)
+                    .categoryId(this.categoryId)
+                    .userId(this.userId)
+                    .build();
+        }
+
+        public static ProductInfo fromEntity(final Product product) {
+            return ProductInfo.builder()
+                    .title(product.getTitle())
+                    .content(product.getContent())
+                    .image(product.getImage())
+                    .productStatus(product.getProductStatus())
+                    .location(product.getLocation())
+                    .count(product.getCount())
+                    .price(product.getPrice())
+                    .userId(product.getUserId())
+                    .categoryId(product.getCategoryId())
+                    .build();
+        }
+    }
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class uploadInfo {
+        private String title;
+        private String content;
+        private String image;
+        private String productStatus; // Enum 으로 변경 예정
+        private String location;
+        private Integer count;
+        private Integer price;
+        private Integer categoryId;
+        private Integer userId;
+
+        MultipartFile productPicture;
 
         public Product toEntity() {
             return Product.builder()
