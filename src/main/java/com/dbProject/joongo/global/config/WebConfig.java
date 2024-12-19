90049321abe41e80f4a25c53cef54a5bcd404d81
heehun <slrkqk2@naver.com>
2024-12-19 17:01:03 +0900

    add

--- src/main/java/com/dbProject/joongo/global/config/WebConfig.java
+++ src/main/java/com/dbProject/joongo/global/config/WebConfig.java
@@ -20,7 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
 //                .excludePathPatterns("/api/categories/**")
                 .excludePathPatterns("/api/auth/**")
                 .excludePathPatterns("/health")
-                .excludePathPatterns("/")
+                .excludePathPatterns("/**")
 //                .excludePathPatterns("/favicon.ico")
                 .excludePathPatterns("/api/products/")
                 .excludePathPatterns("/api/products/{categoryId}")
